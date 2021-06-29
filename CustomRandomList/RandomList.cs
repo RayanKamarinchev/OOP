867113ea13273eaa52c69b376ea182a0d212b5f9
RayanKamarinchev <rayan2008bg@gmail.com>
2021-06-29 17:18:16 +0300

    Inheritance lection passed

---
+++ CustomRandomList/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public Random rnd { get; set; }
+
+        public string RandomString()
+        {
+            int index = rnd.Next(0, this.Count);
+            string result = this[index];
+            this.RemoveAt(index);
+            return result;
+        }
+    }
+}
