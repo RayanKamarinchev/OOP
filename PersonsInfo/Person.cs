1816b81f5e6c493fd7cd311e8d8fcf659501ce0e
RayanKamarinchev <rayan2008bg@gmail.com>
2021-07-02 15:58:44 +0300

    Encapsulation Lab

---
+++ PersonsInfo/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public string FirstName {
+            get => firstName;
+            private set
+            {
+                if (value.Length<3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+        public string LastName 
+        {
+            get => lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+        }
+        public int Age 
+        {
+            get => age;
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary 
+        {
+            get => salary;
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }                    
+                salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            percentage /= 100;
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+            Salary += Salary*percentage;
+        }
+        /* ex 1
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+        */
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+    }
+}
